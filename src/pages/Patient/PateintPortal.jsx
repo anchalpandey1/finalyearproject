99543b4e19e17c319fecdd165774ab58005c077b
nikitapandey2 <nikitapandey9190@gmail.com>
2025-03-28 18:09:36 +0530

    some code added

--- src/pages/Patient/PateintPortal.jsx
+++ src/pages/Patient/PateintPortal.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Footer from '../../components/Footer/FOOTER.JSX'
+import Footer from '../../components/Footer/Footer.jsx'
 import Navbar from '../../components/Navbar/Navbar'
 import SkillsRow from '../../components/Teens/SkillsRow'
 import Patient from '../../components/Patient/Patient'
